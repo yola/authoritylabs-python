cc21f38a48b431206d76fe18636d1725e991f564
William Stewart <william@synthasite.com>
2015-04-03 01:39:57 +0200

    Fix default auth_token parameter

--- authoritylabs/services.py
+++ authoritylabs/services.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from demands import HTTPServiceClient
 
+from authoritylabs.utils import memoized_ttl
+
 
 class AuthorityLabsPartnerApi(HTTPServiceClient):
     def __init__(self, api_key, account_id, url=None, **kwargs):
         url = url or 'http://api.authoritylabs.com'
         super(AuthorityLabsPartnerApi, self).__init__(
-            url, auth_token=api_key, **kwargs)
+            url, params={'auth_token': api_key}, **kwargs)
         self.api_key = api_key
         self.account_id = account_id
 
